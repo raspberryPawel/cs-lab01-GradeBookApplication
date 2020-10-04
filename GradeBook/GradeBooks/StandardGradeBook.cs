5f14a609e279cd77ef2edffd45d3a00f632fefd0
Pawel Malina <pawel.malina1999@gmail.com>
2020-10-04 17:19:46 +0200

    first test

---
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GradeBook.Enums;
+using GradeBook.GradeBooks;
+
+namespace GradeBook.GradeBooks
+{
+    class StandardGradeBook : BaseGradeBook
+    {
+        public StandardGradeBook(string name) : base(name)
+        {
+            Name = name;
+            Students = new List<Student>();
+        }
+    }
+}
